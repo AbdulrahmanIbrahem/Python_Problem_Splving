dac8467063e8e111fdb3a52038036bc8521f7722
AbdulrahmanIbraheem <shaweeshabdulrahmahn@gmail.com>
2022-10-19 19:17:22 +0800

    Calories Burned

---
+++ calories_burned.py
@@ -0,0 +1,10 @@
+
+
+""" Calories Burned """
+
+
+for calory in range(10,31, 5) :
+    
+    total_calories = calory * 4.2 # calculate the total number of calcories will burned in 10, 15, 20, 25 and 30 mintes.
+    
+    print(f'The total number of calories burned in {calory} is {total_calories} Calories')
