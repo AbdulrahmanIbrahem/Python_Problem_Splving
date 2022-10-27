8affee0f61a8112d0e4ba536a9090b5446efac19
AbdulrahmanIbraheem <shaweeshabdulrahmahn@gmail.com>
2022-10-27 08:55:39 +0800

    Pattern

---
+++ pattern.py
@@ -0,0 +1,15 @@
+
+
+
+for iner in range(7,0,-1) :
+    
+    print(iner*'*')
+    
+
+for iner in range(6) :
+    print( '#', end='')
+    for space in range(iner) :
+        
+        print(' ', end='')
+    print('#')
+    
